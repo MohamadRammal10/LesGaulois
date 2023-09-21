bf4b7ef2ad4a31dceea3e76505fcb22fa48d544e
Mohamad Rammal <rmm2787b@ens.fsi>
2023-09-21 12:00:13 +0200

    boulot fini

---
+++ src/personnages/Romain.java
@@ -0,0 +1,15 @@
+package personnages;
+
+public class Romain {
+
+	public String getNom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void recevoirCoup(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
