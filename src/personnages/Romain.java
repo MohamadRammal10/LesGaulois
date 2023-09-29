d2378e6bd94712af70a29ee60c08cb9ef7903345
Mohamad Rammal <rmm2787b@ens.fsi>
2023-09-29 09:46:07 +0200

    village termine

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,14 +1,33 @@
 package personnages;
 
 public class Romain {
-
-	public String getNom() {
-		// TODO Auto-generated method stub
-		return null;
+	private String nom;
+	private int force;
+	
+	public Romain (String nom, int force) {
+		this.nom = nom;
+		this.force = force;
 	}
 
-	public void recevoirCoup(int i) {
-		// TODO Auto-generated method stub
+	public String getNom() {
+		return nom;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+	
+	private String prendreParole() {
+		return "Le romain " + nom + " : ";
+	}
+
+	public void recevoirCoup(int forceCoup) {
+		force -= forceCoup;
+		if (force > 0) {
+			parler("Aie !");
+		} else {
+			parler("J'abandonne...");
+		}
 		
 	}
 
---
+++ src/personnages/Village.java
@@ -0,0 +1,54 @@
+package personnages;
+
+public class Village {
+	private String nom;
+	//private Chef chef;
+	private int nbVillageois = 0;
+	private Gaulois [] villageois;
+	private Chef chef;
+	
+	public Village(String nom, int nbVillageoisMaximum) {
+		this.nom = nom;
+		villageois = new Gaulois[nbVillageoisMaximum];
+	}
+	
+	public void setChef(Chef chef) {
+		this.chef = chef;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void ajouterHabitant(Gaulois gaulois) {
+		villageois[nbVillageois] = gaulois;
+		nbVillageois++;
+	}
+	
+	public Gaulois trouverHabitant(int numeroVillageois) {
+		return villageois[numeroVillageois];
+	}
+	
+	public static void afficherVillageois(Village village) {
+		System.out.println("Dans village du chef " + village.chef.getNom() + " vivent les légendaires gaulois :");
+		for (int i = 0; i < village.nbVillageois; i++) {
+			System.out.println("- " + village.trouverHabitant(i).getNom());
+		}
+	}
+	
+	public static void main(String[] args) {
+		Village village = new Village("Village des Irréductibles", 30);
+		// Gaulois gaulois = village.trouverHabitant(1);
+		// Il n'y a toujours pas d'habitant dans le village
+		Chef chef = new Chef("Abraracourcix", 6, village);
+		village.setChef(chef);
+		Gaulois gaulois0 = new Gaulois("Astérix", 8);
+		village.ajouterHabitant(gaulois0);
+		// Gaulois gaulois = village.trouverHabitant(1);
+		// System.out.println(gaulois);
+		// Gaulois n'est pas un string our qu'il puisse l'afficher il faut print le nom du chef
+		Gaulois gaulois1 = new Gaulois("Obélix", 25);
+		village.ajouterHabitant(gaulois1);
+		afficherVillageois(village);
+	}
+}
